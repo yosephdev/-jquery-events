e206817a6affb963d050fe91a83c2bf65b35dbd8
yosephdev <yosephbet@gmail.com>
2024-03-27 10:38:33 +0000

    Add mouseenter and mouseleave event handler on body element

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
-$(document).ready(function() {
- 
+$(document).ready(function () {
 
- 	$("#stream1_btn").on("click", function() {
- 		$(".stream1").removeClass('highlight_stream');
-		$(".stream2").removeClass('highlight_stream');
-		$(".stream3").removeClass('highlight_stream');
-	  	$(".stream1").addClass('highlight_stream');
-	});
-	$("#stream2_btn").on("click", function() {
+
+	$("#stream1_btn").on("click", function () {
 		$(".stream1").removeClass('highlight_stream');
 		$(".stream2").removeClass('highlight_stream');
 		$(".stream3").removeClass('highlight_stream');
-	  	$(".stream2").addClass('highlight_stream');
+		$(".stream1").addClass('highlight_stream');
 	});
-	$("#stream3_btn").on("click", function() {
+	$("#stream2_btn").on("click", function () {
 		$(".stream1").removeClass('highlight_stream');
 		$(".stream2").removeClass('highlight_stream');
 		$(".stream3").removeClass('highlight_stream');
-	  	$(".stream3").addClass('highlight_stream');
+		$(".stream2").addClass('highlight_stream');
+	});
+	$("#stream3_btn").on("click", function () {
+		$(".stream1").removeClass('highlight_stream');
+		$(".stream2").removeClass('highlight_stream');
+		$(".stream3").removeClass('highlight_stream');
+		$(".stream3").addClass('highlight_stream');
+	});
+
+	$("body").mouseenter(function () {
+		$("body").css("background-color", "#81bbc9d9");
+	});
+
+	$("body").mouseleave(function () {
+		$("body").css("background-color", "#e1e2e2");
 	});
 
 
